e5bb851006e0704e8b527b1cf56d1ba3fb849cef
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-23 15:24:29 -0700

    new added

---
+++ test/headTest.js
@@ -0,0 +1,7 @@
+const head = require('../head');
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1, 2, 3]), 1);
+assertEqual(head(["a", "b", "c"]), "a");
+assertEqual(head([]), undefined);
