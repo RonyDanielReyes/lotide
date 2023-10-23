6c8606b99f669a95a4e023e6dbb0610bd1fa490e
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-23 15:24:47 -0700

    new added

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,5 @@
+
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🙂🙂🙂 Assertion Passed: ${actual} === ${expected}`);
@@ -7,5 +9,7 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+/* assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1); */
+
+module.exports = assertEqual; 
--- head.js
+++ head.js
@@ -1,14 +1,11 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
   return arr[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙂🙂🙂 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+
 
 // TEST CASES
 assertEqual(head([5,6,7]), 5);
