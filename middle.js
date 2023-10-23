761abf978dfc1c1f8640fb0fd2ea328f0a639373
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-23 16:17:03 -0700

    1

--- middle.js
+++ middle.js
@@ -36,9 +36,6 @@ const middle = function(array) {
 };
 
 // Test cases
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = eqArrays;
+module.exports = assertArraysEqual;
+module.exports = middle; s
