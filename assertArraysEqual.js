f3dcdd3beeef57a9be3ffe3b4a9e8de5de37872a
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-23 16:07:37 -0700

    M

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,3 @@
-/* const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙂🙂🙂 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
- */
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -27,8 +19,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-// Test cases
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = eqArrays;
+module.exports = assertArraysEqual;
