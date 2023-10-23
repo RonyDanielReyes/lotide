770df337ed82452bba38ebe2ad10abd976939aa9
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-23 15:57:04 -0700

    hw

---
+++ test/eaArraysTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../eaArrays');
+const eqArrays = require('../eaArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
