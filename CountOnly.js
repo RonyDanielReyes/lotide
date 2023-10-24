16e48356dc1d185948ed657f6e1cd2dfa2ff897a
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-24 08:57:22 -0700

    hol

---
+++ CountOnly.js
@@ -0,0 +1,42 @@
+const countOnly = function(allItems, itemsToCount) {
+  const result = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+
+  return result;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂🙂🙂 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
---
+++ assertObjectsEqual.js
@@ -0,0 +1,34 @@
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// Paste the assertObjectsEqual implementation here
+
+// Example usage:
+const obj1 = { a: '1', b: 2 };
+const obj2 = { b: 2, a: '1' };
+
+assertObjectsEqual(obj1, obj2); // This should print a passing message
+
