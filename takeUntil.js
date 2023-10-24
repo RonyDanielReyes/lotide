ae34254154bddc4981a6719795bc8087e77c7e90
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-23 21:31:50 -0700

    n

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (const item of array) {
+    if (!callback(item)) {
+      result.push(item);
+    } else {
+      break;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const areEqual = eqArrays(arr1, arr2);
+  if (areEqual) {
+    console.log(`🙂🙂🙂 Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`😭😭😭 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);  // Output: [ 1, 2, 5, 7, 2 ]
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);  // Output: [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
